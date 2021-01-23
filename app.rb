a1831d98d31db10e9be0dc05d2cd26db479f21a4
Sierra Wallace <sskwallace@gmail.com>
2021-01-23 23:21:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,6 +22,19 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
-  
+  get '/:operation/:number1/:number2' do
+    @oper = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
     
 end
